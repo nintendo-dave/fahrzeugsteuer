ab53af67c9c5b2ec76d47d5c9d6424b74b5ee4f6
Zuccerberg <davi.bosch@outlook.com>
2020-12-10 14:30:42 +0100

    Added junit. 4.13

--- src/ch/fahrzeugsteuer/Main.java
+++ src/ch/fahrzeugsteuer/Main.java
@@ -1,9 +1,9 @@
 package ch.fahrzeugsteuer;
-import static org.junit.jupiter.api.Assertions;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 
 
