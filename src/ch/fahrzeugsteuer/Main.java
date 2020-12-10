badd694492f8c2a3bc162f6f703aff2acdc08552
Zuccerberg <davi.bosch@outlook.com>
2020-12-10 14:13:48 +0100

    MEthode haha

--- src/ch/fahrzeugsteuer/Main.java
+++ src/ch/fahrzeugsteuer/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+
+    privat void printSomething(){
+        System.out.println("Printed Something haha");
+    }
 }
