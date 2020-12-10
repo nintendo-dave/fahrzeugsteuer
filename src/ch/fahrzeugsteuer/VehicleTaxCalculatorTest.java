b6362145bf3cbbb5987fc6240d807ed8450641f8
gi-oel <gioele.petrillo@gmail.com>
2020-12-10 15:24:09 +0100

    Tests

--- src/ch/fahrzeugsteuer/VehicleTaxCalculatorTest.java
+++ src/ch/fahrzeugsteuer/VehicleTaxCalculatorTest.java
@@ -1,7 +1,15 @@
 package ch.fahrzeugsteuer;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTaxCalculatorTest {
+
+    @Test
+    void is6000() {
+        VehicleTaxCalculator vh = new VehicleTaxCalculator();
+        assertEquals(1258, vh.getPriceFromCcm(6000));
+    }
 
 }
