45c317f620aa8f8eb112a432fd0d19a6ee28be08
gi-oel <gioele.petrillo@gmail.com>
2020-12-10 16:06:13 +0100

    Test verbesserung

--- src/ch/fahrzeugsteuer/VehicleTaxCalculatorTest.java
+++ src/ch/fahrzeugsteuer/VehicleTaxCalculatorTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTaxCalculatorTest {
 
-    int ccm = 8000;
+    int ccm = 6102;
     int expected = 1858;
 
     @Test
@@ -19,6 +19,9 @@ class VehicleTaxCalculatorTest {
     void isBiggerThan6000() {
         int newValue = (ccm - 6000);
         double devided = Math.floor(newValue / 1000);
+        if(!(ccm % 1000 == 0)){
+            devided += 1;
+        }
         double  multiplier = devided * 300 + 1258;
         VehicleTaxCalculator vh = new VehicleTaxCalculator();
         assertEquals(multiplier, vh.getPriceFromCcm(ccm));
